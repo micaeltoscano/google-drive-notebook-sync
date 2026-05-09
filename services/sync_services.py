24a039c42c5092215facff07efe8f2cf6821fdff
Micael Toscano <cont.micaeltoscano@gmail.com>
2026-05-09 18:03:33 -0300

    Update sync_services.py

--- services/sync_services.py
+++ services/sync_services.py
@@ -3,6 +3,7 @@ from sqlalchemy.future import select
 from models.model import Files
 from sqlalchemy.ext.asyncio import AsyncSession
 import asyncio
+from datetime import datetime
 
 
 async def notebook_exists(session: AsyncSession, title: str) -> bool:
@@ -16,12 +17,17 @@ async def sync_notebooks(session: AsyncSession):
 
     for file in files:
 
-        if file["name"].endswith(".ipynb"):
+        if file["name"].endswith(".ipynb") and not await notebook_exists(session, file["name"]): 
 
             content = await asyncio.to_thread(_get_notebook_content, file["id"])
 
             with open(f"notebooks/{file['name']}", "wb") as f:
                 f.write(content)
+
+            modified_at = datetime.fromisoformat(file["modifiedTime"].replace("Z", "+00:00")).replace(tzinfo=None)
+            new_file = Files(title=file["name"], modified_at=modified_at)
+            session.add(new_file)
+            await session.commit()
 
 if __name__ == "__main__":
     asyncio.run(sync_notebooks(None))
