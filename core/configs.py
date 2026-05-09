cb7b9d13614860e76a2d762b9cc15587e7d289b7
Micael Toscano <cont.micaeltoscano@gmail.com>
2026-05-09 13:09:44 -0300

    Update configs.py

--- core/configs.py
+++ core/configs.py
@@ -1,18 +1,20 @@
+from typing import ClassVar
+
 from pydantic_settings import BaseSettings
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 
-class Settings(BaseSettings): 
+class Settings(BaseSettings):
 
-#BaseSettings is used to read environment variables, validate confs, centralize application settings
-    
-    API_V1_STR: str = '/api/v1'
+    API_V1_STR: str = "/api/v1"
+
     DATABASE_URL: str
-    DBBaseModel = declarative_base()
+
+    DBBaseModel: ClassVar = declarative_base()
 
     class Config:
-
         case_sensitive = True
         env_file = ".env"
+
 
 settings = Settings()
